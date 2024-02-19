14933a8ad4d5671361cb81fd03774846a53083b5
bmester <mester.bence15@gmail.com>
2024-02-19 11:24:35 +0100

    initial commit spring

---
+++ backend/UserManager/src/test/java/hu/bmester/usermanager/UserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bmester.usermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
